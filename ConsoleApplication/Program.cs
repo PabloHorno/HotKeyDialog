36bb2edb750f6da79276d6e780148d3b0d417032
trpphp <trpphp@LTTRPPHP>
2018-09-21 12:49:20 +0200

    Removed some inecesary code and an examples added

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -17,22 +17,5 @@ namespace ConsoleApplication
                 Example2();
             }
         }
-        static void Example1()
-        {
-            var e = HotKeyMessageBox.Show("Title", "A simple description");
-            Console.WriteLine("You have pressed the keys: " + e.ToString());
-            Console.ReadKey();
-        }
-        static void Example2()
-        {
-            var hotKey1 = new HotKey();
-            var hotKey2 = new HotKey();
-            
-            hotKey1 = HotKeyMessageBox.Show("Title", "Hotkey 1");
-            hotKey2 = HotKeyMessageBox.Show("Title", "Hotkey 2");
-            Console.WriteLine("You have pressed the keys: " + hotKey1.ToString() + " and " + hotKey2.ToString() + " are they the same? " + (hotKey1 == hotKey2 ? "Yes" : "No"));
-            Console.ReadKey();
-
-        }
     }
 }
--- WindowsFormsTestApplication/Form1.cs
+++ WindowsFormsTestApplication/Form1.cs
@@ -20,7 +20,24 @@ namespace WindowsFormsTestApplication
 
         private void button1_Click(object sender, EventArgs e)
         {
-            label1.Text = HotKeyMessageBox.Show(this.Text, "Pulsa una combinacion de teclas").ToString();
+            Example1();
+        }
+
+        void Example1()
+        {
+            HotKey hotkey = new HotKey();
+            hotkey = HotKeyMessageBox.Show("Title", "Please press the key combination");
+            label1.Text = "You have pressed the keys: " + hotkey.ToString();
+        }
+        void Example2()
+        {
+            var hotKey1 = new HotKey();
+            var hotKey2 = new HotKey();
+
+            hotKey1 = HotKeyMessageBox.Show("Title", "Hotkey 1");
+            hotKey2 = HotKeyMessageBox.Show("Title", "Hotkey 2");
+            label1.Text = "You have pressed the keys: " + hotKey1.ToString() + " and " + hotKey2.ToString() + ". Are they the same? " + (hotKey1 == hotKey2 ? "Yes" : "No");
+
         }
     }
 }
